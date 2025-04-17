9fd8ff2e645617d7161d6527e1d9a871d5b230d3
evelioexcellenta <velioexcel@gmail.com>
2025-04-17 13:47:26 +0700

    idk

--- src/main/java/id/co/bsi/e_walled/model/User.java
+++ src/main/java/id/co/bsi/e_walled/model/User.java
@@ -10,9 +10,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 20)
     private String username;
+
+    @Column(nullable = false, length = 70)
     private String fullname;
+
+    @Column(nullable = false, unique = true, length = 255)
     private String email;
+
+    @Column(nullable = false, columnDefinition = "text")
     private String password;
+
+    @Column(name = "avatar_url", columnDefinition = "text")
     private String avatarUrl; // optional
+
+    
 }
